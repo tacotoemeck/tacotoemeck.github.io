dcce9287995b9fd4c2b4be2ae0018211527cd64c
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-10-22 17:48:53 +0100

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,33 @@
+
+
+let i = 0;
+
+for ( let li of portfolioCarousel.querySelectorAll('li')) {
+    li.style.position = 'relative';
+    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+    i++;
+};
+
+// configuration 
+
+let width = 432;
+let count = 1;
+
+let list = portfolioCarousel.querySelector('ul');
+let listElems = portfolioCarousel.querySelectorAll('li');
+
+let position = 0; 
+
+portfolioCarousel.querySelector('.prev').onclick = function() {
+    //shift left 
+    position += width * count;
+    position = Math.min(position, 0); 
+    list.style.marginLeft = position + 'px';
+};
+    portfolioCarousel.querySelector('.next').onclick = function() {
+    // shift right
+    position -= width * count;
+    // can only shift the ribbbon for (total ribbon length - visible count) images
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+};
---
+++ navbar.js
@@ -0,0 +1,29 @@
+const burgerIcon = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const navLinksHidden = document.getElementsByClassName('hiddenNav') + 25;
+const landingSection = document.querySelector('.landing');
+
+const navLinksClientWidth = navLinks.clientWidth;
+
+navLinks.style.marginRight = `-${navLinksClientWidth}` + `px`;
+const navHiddenMargin = navLinks.style.marginRight;
+
+let menuShowing = false;
+
+
+burgerIcon.addEventListener('click', openNavBar);
+
+function openNavBar() {
+    if ( menuShowing ==  false ) {
+    navLinks.style.marginRight = `35px`;
+    
+    burgerIcon.classList.add('toggle');
+    menuShowing = true;
+    }
+    else {
+    navLinks.style.marginRight = navHiddenMargin; 
+    burgerIcon.classList.remove('toggle');
+    
+    menuShowing = false;
+    }
+}
